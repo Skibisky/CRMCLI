aa1139d102cd39e44e7703a39a617cca6526db18
Samuel Warnock <samuel.warnock@illuminancesolutions.com.au>
2017-08-10 15:44:56 +0800

    Added a customisation comparer for left/right orgs.

---
+++ CustomisationComparison/CRMSerializer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+using System.Xml;
+using System.Collections;
+
+namespace CEC.CustomisationComparer.DCE
+{
+	public abstract class CRMSerializer
+	{
+		public abstract void Write(XmlDocument rootDoc, object o, string name = null, XmlElement currentElement = null);
+
+		public static Type GetSerializer(Type type)
+		{
+			var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(CRMSerializer).IsAssignableFrom(t));
+			types = types.Concat(Assembly.GetCallingAssembly().GetTypes().Where(t => typeof(CRMSerializer).IsAssignableFrom(t)));
+			var tt = types.Where(t => t.BaseType.IsConstructedGenericType && t.BaseType.GenericTypeArguments.First() == type);
+			return tt?.FirstOrDefault() ?? typeof(ObjectSerializer);
+		}
+
+		static List<int> test = new List<int>();
+
+		public static void Serialize(XmlDocument rootDoc, object o, string name = null, XmlElement currentElement = null)
+		{
+			if (o == null)
+				return;
+			if (test.Contains(o.GetHashCode()))
+				return;
+			test.Add(o.GetHashCode());
+			var wr = GetSerializer(o.GetType());
+			var ds = (CRMSerializer)wr.GetConstructor(new Type[] { }).Invoke(null);
+			ds.Write(rootDoc, o, name, currentElement);
+		}
+	}
+
+	public abstract class CRMSerializer<T> : CRMSerializer
+	{
+	}
+
+	public class ObjectSerializer : CRMSerializer<object>
+	{
+		public override void Write(XmlDocument rootDoc, object o, string name = null, XmlElement currentElement = null)
+		{
+			name = (name ?? o.GetType().ToString()).Replace("<", "").Replace(">", "").Replace("[", "").Replace("]", "");
+			if (o.GetType().IsValueType)
+			{
+				currentElement.SetAttribute(name, o.ToString());
+			}
+			else
+			{
+				XmlElement xe = rootDoc.CreateElement(name);
+				if (currentElement != null)
+					currentElement.AppendChild(xe);
+				else
+					rootDoc.AppendChild(xe);
+
+				if (o.GetType().GetInterfaces().Contains(typeof(IEnumerable)))
+				{
+					foreach (var thing in (IEnumerable)o)
+					{
+						CRMSerializer.Serialize(rootDoc, thing, null, xe);
+					}
+				}
+				else
+				{
+					var fields = o.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+					foreach (var thing in fields)
+					{
+						CRMSerializer.Serialize(rootDoc, thing.GetValue(o), thing.Name, xe);
+					}
+					var prop = o.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+					foreach (var thing in prop)
+					{
+						CRMSerializer.Serialize(rootDoc, thing.GetValue(o), thing.Name, xe);
+					}
+				}
+			}
+
+		}
+	}
+
+	public class StringSerializer : CRMSerializer<string>
+	{
+		public override void Write(XmlDocument rootDoc, object o, string name = null, XmlElement currentElement = null)
+		{
+			name = (name ?? o.GetType().ToString()).Replace("<", "").Replace(">", "").Replace("[", "").Replace("]", "");
+			var xe = rootDoc.CreateElement(name);
+			xe.InnerText = o.ToString();
+			if (currentElement != null)
+				currentElement.AppendChild(xe);
+			else
+				rootDoc.AppendChild(xe);
+		}
+	}
+
+
+}
---
+++ CustomisationComparison/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
+using Microsoft.Xrm.Sdk;
+using CEC.CustomisationComparer.DCE;
+
+namespace CEC.CustomisationComparer.CLI
+{
+	class Test
+	{
+		int value = 3;
+		public string key = "topkek";
+		public KeyValuePair<string, int> rep = new KeyValuePair<string, int>("topkek", 3);
+	}
+
+	class EntityMetaDataSerializer : CRMSerializer<EntityMetadata>
+	{
+		public override void Write(XmlDocument rootDoc, object o, string name = null, XmlElement currentElement = null)
+		{
+			var m = (EntityMetadata)o;
+			var xe = rootDoc.CreateElement("Entity");
+			if (currentElement == null)
+				rootDoc.AppendChild(xe);
+			else
+				currentElement.AppendChild(xe);
+
+			var e = rootDoc.CreateElement("ObjectTypeCode");
+			e.InnerText = m.ObjectTypeCode.ToString();
+			xe.AppendChild(e);
+			
+		}
+	}
+
+
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			var org1 = CEC.Extensions.ExtensionMethods.Connect("http://iscrmdev2013.cloudapp.net/IS2", "samuel.warnock", "80Barrack");
+
+			var me = ((WhoAmIResponse)org1.Execute(new WhoAmIRequest())).UserId;
+
+
+
+			/**/
+			var resp = org1.Execute(new RetrieveAllEntitiesRequest()
+			{
+				EntityFilters = EntityFilters.All
+			});
+			var res1 = resp.Results["EntityMetadata"];
+
+
+			XmlDocument doc = new XmlDocument();
+			using (TextWriter tw = new StringWriter())
+			using (XmlWriter xw = XmlWriter.Create(tw, new XmlWriterSettings()
+			{
+				Indent = true,
+				IndentChars = "\t"
+			}))
+			{
+				CRMSerializer.Serialize(doc, res1, "MetaData");
+				doc.WriteTo(xw);
+				xw.Flush();
+				Console.WriteLine(tw.ToString());
+				File.WriteAllText("sol.xml", tw.ToString());
+			}
+
+			Console.ReadKey();
+			return;
+
+			
+			/**/
+
+			List<EntityReference> forms = new List<EntityReference>();
+			for (int i = 0; i < 10; i++)
+			{
+
+				var res1b = (RetrieveFilteredFormsResponse)org1.Execute(new RetrieveFilteredFormsRequest()
+				{
+					EntityLogicalName = "incident",
+					FormType = new Microsoft.Xrm.Sdk.OptionSetValue(i),
+					SystemUserId = me
+				});
+				forms.AddRange(res1b.SystemForms);
+			}
+			foreach (var er in forms)
+			{
+				var e = org1.Retrieve(er.LogicalName, er.Id, new ColumnSet(true));
+			}
+		}
+	}
+}
